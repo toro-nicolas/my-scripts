60ee3a2f17d0fb1d002d2471bd3713a058515972
Nicolas TORO <nicolas.toro@epitech.eu>
2026-02-12 14:11:33 +0100

    [~] Refactor sfml-switch.sh to use SFML_DIRECTORY variable and add sudo to file operations

--- sfml-switch.sh
+++ sfml-switch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VERSION_LIST=$(ls -d sfml/*/ | xargs -n 1 basename)
+SFML_DIRECTORY=$HOME/my_scripts/sfml
+VERSION_LIST=$(ls -d "$SFML_DIRECTORY"/*/ | xargs -n 1 basename)
 
 if [ $# -ne 1 ]; then
     echo "Usage: sudo $0 <version>"
@@ -10,7 +11,7 @@ fi
 
 VERSION=$1
 
-if [ ! -d "sfml/$VERSION" ]; then
+if [ ! -d "$SFML_DIRECTORY/$VERSION" ]; then
     echo "Error: version '$VERSION' not available."
     echo "Available versions: $(echo $VERSION_LIST | tr ' ' ', ')"
     exit 1
@@ -20,23 +21,23 @@ echo "Switching SFML version to $VERSION..."
 
 # Cleanup files
 echo "Cleaning up old SFML files..."
-rm -rf /usr/lib/cmake/SFML
-rm -rf /usr/lib/cmake/CSFML
-rm -rf /usr/include/SFML
-rm -rf /usr/include/CSFML
-rm -rf /usr/lib/libcsfml*.so*
-rm -rf /usr/lib/libsfml*.so*
-rm -rf /usr/lib/pckgconfig/sfml-*.pc
-rm -rf /usr/lib/pckgconfig/csfml-*.pc
+sudo rm -rf /usr/lib/cmake/SFML
+sudo rm -rf /usr/lib/cmake/CSFML
+sudo rm -rf /usr/include/SFML
+sudo rm -rf /usr/include/CSFML
+sudo rm -rf /usr/lib/libcsfml*.so*
+sudo rm -rf /usr/lib/libsfml*.so*
+sudo rm -rf /usr/lib/pckgconfig/sfml-*.pc
+sudo rm -rf /usr/lib/pckgconfig/csfml-*.pc
 
 # Copy new files
 echo "Copying new SFML files..."
-cp -Pr sfml/$VERSION/cmake/* /usr/lib/
-cp -Pr sfml/$VERSION/include/* /usr/include/
-cp -P sfml/$VERSION/lib/* /usr/lib/
-cp -P sfml/$VERSION/pkgconfig/* /usr/lib/pkgconfig/
+sudo cp -Pr $SFML_DIRECTORY/$VERSION/cmake/* /usr/lib/
+sudo cp -Pr $SFML_DIRECTORY/$VERSION/include/* /usr/include/
+sudo cp -P $SFML_DIRECTORY/$VERSION/lib/* /usr/lib/
+sudo cp -P $SFML_DIRECTORY/$VERSION/pkgconfig/* /usr/lib/pkgconfig/
 
 # Reload library cache
-ldconfig
+sudo ldconfig
 
 echo "Switched SFML version to $VERSION"
